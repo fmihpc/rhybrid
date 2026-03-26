cb2cd76007a2a33cbf24ce58112e3c87252e87b5
rjarvinen <riku.jarvinen@fmi.fi>
2026-03-26 21:01:12 +0200

    removed shock tube configuration

--- src/user.cpp
+++ src/user.cpp
@@ -1935,16 +1935,6 @@ bool userLateInitialization(Simulation& sim,SimulationClasses& simClasses,Config
             const Real xNode = crd[b3+0] + (i+1.0)*Hybrid::dx;
 	    const Real yNode = crd[b3+1] + (j+1.0)*Hybrid::dx;
 	    const Real zNode = crd[b3+2] + (k+1.0)*Hybrid::dx;
-#ifdef USE_SHOCKTUBE_TEST_CONFIGURATION
-	    // initial shocktube setup
-	    if (xCellCenter >= 0.5e5) {
-	       faceB[n*3+1] = 1e-9;
-	    }
-	    else {
-	       faceB[n*3+1] = -1e-9;
-	    }
-	    faceB[n*3+0] = 1.5e-9;
-#endif
 	    const Real r2 = sqr(xCellCenter) + sqr(yCellCenter) + sqr(zCellCenter);
 	    if (r2 < Hybrid::R2_fieldObstacle) { innerFlagField[n] = true; }
 	    else                              { innerFlagField[n] = false; }
@@ -2149,7 +2139,6 @@ bool userLateInitialization(Simulation& sim,SimulationClasses& simClasses,Config
       simClasses.logger << write;
 #endif
 #ifdef USE_B_INITIAL
-#ifndef USE_SHOCKTUBE_TEST_CONFIGURATION
       // set initial B
       for (pargrid::CellID b=0; b<simClasses.pargrid.getNumberOfLocalCells(); ++b) {
          const size_t b3 = 3*b;
@@ -2186,7 +2175,6 @@ bool userLateInitialization(Simulation& sim,SimulationClasses& simClasses,Config
             cellB[n*3+2] = B_initial[2];
          }
       }
-#endif
 #endif
    }
 
