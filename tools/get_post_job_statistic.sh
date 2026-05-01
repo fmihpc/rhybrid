c7b05ad42483db144760ba89f2788ae62b17d375
rjarvinen <riku.jarvinen@fmi.fi>
2026-05-01 22:02:08 +0300

    update to example slurm scripts

--- tools/get_post_job_statistic.sh
+++ tools/get_post_job_statistic.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-output_file="resource_usage.txt"
-date >$output_file 2>&1
-pwd >>$output_file 2>&1
-echo "" >>$output_file 2>&1
 jobid=""
 # try to find a slurm-*.out file
 file=$(ls slurm-*.out 2>/dev/null | head -n 1)
@@ -16,17 +12,54 @@ else
     echo "slurm.*.out nor jobid.txt not found, cannot determine job ID"
     exit 0
 fi
-# check jobid is numberic
+# check jobid is numeric
 [[ $jobid =~ ^[0-9]+$ ]] || {
     echo "error: invalid job ID: $jobid" >&2
     exit 1
 }
+
+output_file="resource_usage_$jobid.txt"
+date >$output_file 2>&1
+pwd >>$output_file 2>&1
+echo "" >>$output_file 2>&1
+
 echo "Job ID: $jobid" >>$output_file 2>&1
+echo "" >>$output_file 2>&1
+
+echo "=======================================================================================" >>$output_file 2>&1
+echo "" >>$output_file 2>&1
+echo "sacct --format=JobID,JobName,Partition,Account,State,ExitCode,Elapsed,Submit,Start,End,AllocCPUS,ReqMem,MaxRSS -j $jobid" >>$output_file 2>&1
+echo "" >>$output_file 2>&1
+sacct --format=JobID,JobName,Partition,Account,State,ExitCode,Elapsed,Submit,Start,End,AllocCPUS,ReqMem,MaxRSS -j $jobid >>$output_file 2>&1
+echo "" >>$output_file 2>&1
+
+echo "=======================================================================================" >>$output_file 2>&1
 echo "" >>$output_file 2>&1
 echo "seff -d $jobid" >>$output_file 2>&1
 echo "" >>$output_file 2>&1
 seff -d $jobid >>$output_file 2>&1
 echo "" >>$output_file 2>&1
+
+echo "=======================================================================================" >>$output_file 2>&1
+echo "" >>$output_file 2>&1
 echo "sacct --long -j $jobid" >>$output_file 2>&1
 echo "" >>$output_file 2>&1
 sacct --long -j $jobid >>$output_file 2>&1
+echo "" >>$output_file 2>&1
+
+echo "=======================================================================================" >>$output_file 2>&1
+echo "" >>$output_file 2>&1
+echo "sacct --format=ALL -j $jobid" >>$output_file 2>&1
+echo "" >>$output_file 2>&1
+sacct --format=ALL -j $jobid >>$output_file 2>&1
+echo "" >>$output_file 2>&1
+
+echo "=======================================================================================" >>$output_file 2>&1
+echo "" >>$output_file 2>&1
+echo "scontrol show job $jobid" >>$output_file 2>&1
+echo "" >>$output_file 2>&1
+scontrol show job $jobid >>$output_file 2>&1
+echo "" >>$output_file 2>&1
+
+echo "=======================================================================================" >>$output_file 2>&1
+
